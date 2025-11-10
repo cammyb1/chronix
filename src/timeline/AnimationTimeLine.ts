b6e15e868e944d7fa230e88bc48a4d6004d029f5
jvasquez <seiya_al_@hotmail.com>
2025-11-11 00:40:38 +0100

    feat: basic stuff for timeline

--- src/timeline/AnimationTimeLine.ts
+++ src/timeline/AnimationTimeLine.ts
@@ -5,20 +5,36 @@ export interface ATLEvents {
   tick: { dt: number };
 }
 
-export class AnimationTimeLine extends EventBus<ATLEvents> {
+export class AnimationTimeLine<T = undefined> extends EventBus<ATLEvents> {
   duration: number;
+  container: HTMLElement;
   tracks: KeyframeTrack[];
+  root: T | undefined;
 
-  constructor() {
+  constructor(container?: HTMLElement) {
     super();
-
     this.duration = 0;
     this.tracks = [];
+
+    if (container) {
+      this.container = container;
+    } else {
+      this.container = document.createElement('div');
+      document.appendChild(this.container);
+    }
+  }
+
+  bind(target: T): this {
+    this.root = target;
+    return this;
+  }
+
+  dispose() {
+    this.container.parentNode?.removeChild(this.container);
   }
 
   setDuration(dur: number): this {
     this.duration = dur;
-
     return this;
   }
 
